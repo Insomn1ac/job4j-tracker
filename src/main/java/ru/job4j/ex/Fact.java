5e81153121c42d90d06ba9c5e9e3d497f02934b0
Insomn1ac <emelyanoff94@gmail.com>
2021-05-22 14:06:37 +0300

    Кидаем исключение - throw new RuntimeException [#219365]

---
+++ src/main/java/ru/job4j/ex/Fact.java
@@ -0,0 +1,18 @@
+package ru.job4j.ex;
+
+public class Fact {
+    public static void main(String[] args) {
+
+    }
+
+    public int calc(int n) {
+        int rsl = 1;
+        if (n < 0) {
+            throw new IllegalArgumentException("Number must be positive or zero");
+        }
+        for (int i = 0; i <= n; i++) {
+            rsl *= i;
+        }
+        return rsl;
+    }
+}
