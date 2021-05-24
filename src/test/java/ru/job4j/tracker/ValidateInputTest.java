590bf36cdae073742c454a59c958d014e2182c2b
Insomn1ac <emelyanoff94@gmail.com>
2021-05-24 13:45:45 +0300

    Тест на ValidateInput [#311887]

---
+++ src/test/java/ru/job4j/tracker/ValidateInputTest.java
@@ -0,0 +1,56 @@
+package ru.job4j.tracker;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.*;
+
+public class ValidateInputTest {
+
+    @Test
+    public void whenInvalidInput() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] {"one", "1"}
+        );
+        ValidateInput input = new ValidateInput(in, out);
+        int selected = input.askInt("Enter menu:");
+        assertThat(selected, is(1));
+    }
+
+    @Test
+    public void whenRightInput() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] {"0"}
+        );
+        ValidateInput input = new ValidateInput(in, out);
+        int selected = input.askInt("Enter menu:");
+        assertThat(selected, is(0));
+    }
+
+    @Test
+    public void whenNegativeInput() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] {"-2"}
+        );
+        ValidateInput input = new ValidateInput(in, out);
+        int selected = input.askInt("Enter menu:");
+        assertThat(selected, is(-2));
+    }
+
+    @Test
+    public void whenMultipleRightInput() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] {"0", "2", "4", "5"}
+        );
+        ValidateInput input = new ValidateInput(in, out);
+        int[] selected = new int[4];
+        for (int i = 0; i < selected.length; i++) {
+            selected[i] = input.askInt("Enter menu:");
+        }
+        assertThat(selected, is(new int[] {0, 2, 4, 5}));
+    }
+}
