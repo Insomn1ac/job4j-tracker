cb4838d6767beda2fc7a6261e4c91864942b3afc
Insomn1ac <emelyanoff94@gmail.com>
2021-06-04 13:04:40 +0300

    Идентичные тексты [#212655]

---
+++ src/main/java/ru/job4j/collection/UniqueText.java
@@ -0,0 +1,18 @@
+package ru.job4j.collection;
+
+import java.util.HashSet;
+
+public class UniqueText {
+    public static boolean isEquals(String originText, String duplicateText) {
+        String[] origin = originText.split(" ");
+        String[] text = duplicateText.split(" ");
+        HashSet<String> check = new HashSet<>();
+        for (String orig : origin) {
+            check.add(orig);
+            for (String duplicate : text) {
+                return check.contains(duplicate);
+            }
+        }
+        return true;
+    }
+}
