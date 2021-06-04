5cd8cde93062984bfc32a1e61d7cc2a96e8929d8
Insomn1ac <emelyanoff94@gmail.com>
2021-06-04 18:56:27 +0300

    Map, HashMap [#212652]

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -1,7 +1,6 @@
 package ru.job4j.collection;
 
 import java.util.HashMap;
-import java.util.Map;
 
 public class UsageMap {
     public static void main(String[] args) {
@@ -9,8 +8,9 @@ public class UsageMap {
         map.put("1234567@mail.ru", "Stepan Vasiliev");
         map.put("qwerty@gmail.com", "Ivan Ivanov");
         map.put("asdfg@outlook.com", "Vasily Petrov");
-        for (Map.Entry<String, String> entry : map.entrySet()) {
-            System.out.println(entry.getValue() + " : " + entry.getKey());
+        for (String key : map.keySet()) {
+            String value = map.get(key);
+            System.out.println(value + " : " + key);
         }
     }
 }
