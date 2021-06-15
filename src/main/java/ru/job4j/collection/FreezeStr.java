4349f3410b28fff4e80c89a9e6727fbe1db01add
Insomn1ac <emelyanoff94@gmail.com>
2021-06-15 14:43:05 +0300

    Экзамен. Коллекции. [#10097 #167527]

--- src/main/java/ru/job4j/collection/FreezeStr.java
+++ src/main/java/ru/job4j/collection/FreezeStr.java
@@ -8,21 +8,22 @@ public class FreezeStr {
         String[] strL = left.split("");
         String[] strR = right.split("");
         Map<String, Integer> mapL = new HashMap<>();
-        Map<String, Integer> mapR = new HashMap<>();
         for (String l : strL) {
-            if (!mapL.containsKey(l)) {
-                mapL.put(l, 1);
-            } else {
-                mapL.put(l, mapL.get(l + 1));
-            }
+            int count = mapL.getOrDefault(l, 0);
+            mapL.put(l, count + 1);
         }
         for (String r : strR) {
-            if (!mapR.containsKey(r)) {
-                mapR.put(r, 1);
-            } else {
-                mapR.put(r, mapR.get(r + 1));
+            Integer value = mapL.get(r);
+            if (!mapL.containsKey(r)) {
+                return false;
+            }
+            if (mapL.containsKey(r) && value == 1) {
+                mapL.remove(r);
+            }
+            if (mapL.containsKey(r) && value > 1) {
+                mapL.put(r, value - 1);
             }
         }
-        return mapL.equals(mapR);
+        return mapL.size() == 0;
     }
 }
