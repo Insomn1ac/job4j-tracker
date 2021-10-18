7b2b96e80355d5c7ec0c519d7d85339424085e18
Insomn1ac <emelyanoff94@gmail.com>
2021-10-18 22:01:55 +0300

    JaCoCo was updated to 0.8.7, @Ignore annotation was removed from 1 test

--- src/test/java/ru/job4j/collection/ConvertList2ArrayTest.java
+++ src/test/java/ru/job4j/collection/ConvertList2ArrayTest.java
@@ -1,15 +1,11 @@
 package ru.job4j.collection;
 
-import org.junit.Ignore;
 import org.junit.Test;
 
-import java.util.Arrays;
 import java.util.List;
 
-import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertThat;
+import static org.junit.Assert.*;
 
-@Ignore
 public class ConvertList2ArrayTest {
     @Test
     public void when7ElementsThen9() {
@@ -23,6 +19,6 @@ public class ConvertList2ArrayTest {
                 {4, 5, 6},
                 {7, 0, 0}
         };
-        assertThat(result, is(expect));
+        assertEquals(result, expect);
     }
 }
